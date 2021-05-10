872aa3c742dc7503a7ec3a27940a37aedb9ccf5f
Denis <denis-yurochkin@mail.ru>
2021-05-10 23:54:40 +0300

    Initial commot.

---
+++ Test_AS_LLS.h
@@ -0,0 +1,18 @@
+#ifndef LAB2_TEST_AS_LLS_H
+#define LAB2_TEST_AS_LLS_H
+
+#endif //LAB2_TEST_AS_LLS_H
+
+float round_float2(float a, int order);
+void as_perfomance_test();
+void ls_perfomance_test();
+//ArraySequence Test
+void constructors_array_sequence();
+void decomposition_array_sequence();
+void function_array_sequence();
+
+//LinkedListSequence Test
+void constructors_listSequence();
+void decomposition_listSequence();
+void function_listSequence();
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "MenuFull.h"
+#include "Tests_full.h"
+int main() {
+
+    tests();
+    menu();
+
+}
